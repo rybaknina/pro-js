67022fd77292759c25cbb446b23aa586c8d7fc9d
Nina Rybak <nrybak@dev.vtb.ru>
2022-01-31 15:33:05 +0300

    task-1: add config for styles to webpack, add some styles

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import getProductList from "./mock/data.js";
+import renderGoodsList from "./showcase.js";
+import "./style/style.scss";
+
+const productList = getProductList(20);
+
+renderGoodsList(productList);
